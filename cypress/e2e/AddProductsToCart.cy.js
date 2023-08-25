2ea52aa7b27a976f0f622a26981d9da29fa1774f
Tomas Sandullo <tomassandullo@gmail.com>
2023-08-24 22:47:26 -0300

    Filter By Cheaper added

--- cypress/e2e/AddProductsToCart.cy.js
+++ cypress/e2e/AddProductsToCart.cy.js
@@ -1,7 +1,13 @@
-describe('Homepage', () => {
-  it('Search for a Motorola EDGE 30 ultra', () => {
+describe('Filter products and add them to the cart', () => {
+  beforeEach('Search for a Motorola EDGE 30 ultra', () => {
     cy.visit('https://www.mercadolibre.com.ar/')
     cy.get('#cb1-edit').type('Motorola EDGE 30 Ultra')
     cy.get('.nav-icon-search').click()
+    cy.get('.andes-button--transparent').click()
+    cy.get('[data-testid="action:understood-button"]').click()
   })
+  it('Filter by cheaper',() => {
+    cy.get('.andes-dropdown__display-values').click()
+    cy.get('[data-key="price_asc"]').click()
+    })
 })
